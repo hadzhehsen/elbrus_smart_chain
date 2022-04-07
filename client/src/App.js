2ed5d985472d2131760b1c34fa85d8b201e1f32f
Mark <smirnovsd19@mail.ru>
2022-04-07 12:58:08 +0300

    navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Faces from "./components/Faces";
+import Home from "./components/Home";
+import Images from "./components/Images";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path="/images" element={<Images/>}/>
+      <Route path="/faces" element={<Faces/>}/>
+      <Route path="/main" element={<Home/>}/>
+    </Routes>
+    </>
   );
 }
 
