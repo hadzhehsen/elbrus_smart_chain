6a36dcbaf9dc5439018d45b98f2abd304705fd4b
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-04-07 16:02:32 +0300

    error modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
-import { Route, Routes } from "react-router-dom";
-import Faces from "./components/Faces";
-import Home from "./components/Home";
-import Images from "./components/Images";
-import Navbar from "./components/Navbar";
+import { Route, Routes } from 'react-router-dom';
+import Faces from './components/Faces';
+import Home from './components/Home';
+import Images from './components/Images';
+import WalletCard from './components/Metamask/WalletCard';
+import WalletCardEthers from './components/Metamask/WalletCardEthers';
+import MetamaskModal from './components/MetamaskModal';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path="/images" element={<Images/>}/>
-      <Route path="/faces" element={<Faces/>}/>
-      <Route path="/main" element={<Home/>}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/images' element={<Images />} />
+        <Route path='/faces' element={<Faces />} />
+        <Route path='/main' element={<Home />} />
+      </Routes>
+      <WalletCard />
+      <WalletCardEthers />
+      <MetamaskModal />
     </>
   );
-};
+}
 
 export default App;
